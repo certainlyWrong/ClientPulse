51729603132c3aade644c29852bdbb35d831f723
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-01-30 01:53:15 -0300

    Update database connection settings

--- clientpulse/db_connection.py
+++ clientpulse/db_connection.py
@@ -4,18 +4,17 @@ from sqlmodel import SQLModel
 from clientpulse.core.controllers.client_controller import ClientController
 import os
 
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
-DATABASE_PORT = os.getenv('DATABASE_PORT', '3302')
-DATABASE_NAME = os.getenv('DATABASE_NAME', 'clientpulse')
-DATABASE_USER = os.getenv('DATABASE_USER', 'root')
+DATABASE_URL = os.getenv('DATABASE_URL', 'localhost:3302')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'stockpulse')
+DATABASE_USER = os.getenv('DATABASE_USER', 'admin')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '123456')
 
 database_url = (f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}"
-                f"@{DATABASE_HOST}:{DATABASE_PORT}")
+                f"@{DATABASE_URL}")
 
 print("database_url", database_url)
-print("DATABASE_HOST", DATABASE_HOST)
-print("DATABASE_PORT", DATABASE_PORT)
+
+print("DATABASE_HOST", DATABASE_URL)
 print("DATABASE_NAME", DATABASE_NAME)
 print("DATABASE_USER", DATABASE_USER)
 print("DATABASE_PASSWORD", DATABASE_PASSWORD)
