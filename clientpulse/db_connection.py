1c19625be0f2a42ce8185bcef9501d298f28a788
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-02-01 03:47:19 -0300

    Add database connection and logging statements

--- clientpulse/db_connection.py
+++ clientpulse/db_connection.py
@@ -11,12 +11,17 @@ engine: Engine
 clientController: ClientController
 
 try:
+    print("Creating database connection...")
     engine = create_engine(
         Enviroments.get_instance.database_connection,
         echo=True,
     )
 
+    print("Connecting to database...")
+
     engine.connect()
+
+    print("Database connected")
 
     SQLModel.metadata.create_all(engine, checkfirst=True)
     clientController = ClientController(engine)
